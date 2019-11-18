045845089d366158b0b147c877e9c6f1ffc134e5
symonsajib <symon.sajib@gmail.com>
2019-11-17 19:36:59 -0500

    Add files via upload

---
+++ Exercise25_Guessing Game Two.py
@@ -0,0 +1,11 @@
+
+
+Number = float(input("What's your number: "))
+Modulus = Number%2
+Modulus_four = Number%4
+if Modulus == 0:
+    print("Even Number.")
+    if Modulus_four == 0:
+        print("It's also multiple of 4 !!")
+else:
+    print("Odd")
