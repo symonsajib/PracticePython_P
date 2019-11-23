e7c1024f9b8b1f8d4e6026dda8a84a04fedffcd5
symonsajib <symon.sajib@gmail.com>
2019-11-23 18:32:11 -0500

    Add files via upload

---
+++ Exercise33_Birthday Dictionaries.py
@@ -0,0 +1,13 @@
+
+birthdays = {
+    "Symon": "11-11-11",
+    "Sam":"12-12-12",
+    "Sai": "13-13-13",
+    "Sevanosky": "14-14-14",
+    "Salam": "15-15-15"
+
+}
+
+print("Here are the list I have" + str(birthdays.keys()))
+name = input("Who's birthday you want to know: ")
+print(birthdays[name])
